d932f58b73540d33c15918e979f924500caab242
denislavrov <lavrov.den@gmail.com>
2023-02-12 21:55:36 +0100

    test

--- client/src/app/component/common/Button/button.jsx
+++ client/src/app/component/common/Button/button.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './button.css';
 
+// test//
+
 const Button = ({ label, className, action, payload, btnType, disabled }) => {
     return (
         action
